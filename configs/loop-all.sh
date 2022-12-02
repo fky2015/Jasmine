f685da917c98d71b1625b6ea450c0a4305626808
Feng Kaiyu <loveress01@outlook.com>
2022-12-03 02:48:27 +0800

    update dataset.

--- configs/loop-all.sh
+++ configs/loop-all.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
+
+#################################
+# 
+# This script loops through all the files (generate by config-gen) in a directory
+# and runs a command on each file.
 #
+# This helps to continuously conduct a series of experiments.
+#
+# Features:
+# 1. The script will find all the runnable folder in the current directory,
+#   and run the command by the order of creation time.
+# 2. All the results are collected to current directory.
+# 3. When timeout, the script will auto retry the experiment.
+#
+# Usage: bash loop-all.sh
+#
+# Author: Feng Kaiyu <loveress01@outlook.com>
+#
+#################################
+
+# Kill current process and retry after $TIMEOUT seconds.
+# This should be larger than the time it takes to run the script normally.
+TIMEOUT=600
+
+# No need to change this
 FILE="run-all.sh"
 
-set -x
+# Run the script, and remove it if it succeeds.
+cmd() {
+  NEXT_FOLDER=$1
+
+  echo "cd $NEXT_FOLDER"
+  cd "$NEXT_FOLDER" || exit 1
+
+  echo "Running $FILE"
+  bash "$FILE"
+
+  echo "cd .."
+  cd ..
+  
+  # move result to folder
+  find "$NEXT_FOLDER" -type f -name "result*.json" -exec mv '{}' . \;
+
+  # remove folder
+  rm -r "$NEXT_FOLDER"
+}
 
 find_dir ()
 {
@@ -19,19 +61,13 @@ find_dir ()
 
   NEXT_FOLDER=${FOLDERS[0]}
 
-  # execute
+  # Wait until timeout
+  cmd "$NEXT_FOLDER" &
+  sleep $TIMEOUT &
+  wait -n
 
-  cd "$NEXT_FOLDER" || exit 1
-
-  bash "$FILE"
-
-  cd ../
-
-  # move result to folder
-  find "$NEXT_FOLDER" -type f -name "result*.json" -exec mv '{}' . \;
-
-  # remove folder
-  rm -r "$NEXT_FOLDER"
+  # Kill all children, continue
+  pkill -P $$
 }
 
 while true; do
--- configs/moving.sh
+++ configs/moving.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+####################################
+#
+# moving.sh
+#
+# This script moves results from current directory to destination, 
+# autoraname the files if conflict.
+#
+# Usage: moving.sh <destination directory>
+#
+####################################
+
 DEST=${1:-../data/5-plans}
 
 echo "moving destination ${DEST}"
@@ -7,13 +18,13 @@ echo "moving destination ${DEST}"
 while true; do
   if compgen -G "result*.json" > /dev/null; then
     for f in $(find . -maxdepth 1 -type f -name "result*.json"); do
-      basename=$(basename $f)
+      basename=$(basename "$f")
       extention="${basename##*.}"
       filename="${basename%.*}"
       # if destination file exists, append a number to the filename
       if [[ -f "$DEST/$basename" ]]; then
         i=1
-        while [[ -f "$DEST/$filename-$i.$extention" ]]; do
+        while [[ -f "$DEST/$filename--$i.$extention" ]]; do
           let i++
         done
         basename="$filename--$i.$extention"
